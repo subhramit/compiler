03e2a80f58fc32acb6f181f5c9a0b81a68578367
subhramit <subhramit.bb@live.in>
2024-02-15 06:28:41 +0530

    modified skeleton

--- driver.c
+++ driver.c
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "src/parser/parserDef.h"
 #include "src/lexer/lexer.h"
+#include "src/parser/parserDef.h"
 #include "src/parser/parser.h"
+
 
 int main(int argc, char* argv[]) {
 
