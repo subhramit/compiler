46e6d98573ab220124c07d5ce9758b0aaeedbee7
subhramit <subhramit.bb@live.in>
2024-03-03 20:56:01 +0530

    renamed files, aded definitions

--- src/parser/TokenSet.h
+++ src/parser/TokenSet.h
@@ -1,5 +1,10 @@
+#ifndef TOKEN_SET_H
+#define TOKEN_SET_H
+
 TokenSet* createTokenSet();
 void destroyTokenSet(TokenSet* ts);
 void addToken(TokenSet* ts, const char* tok);
 void removeToken(TokenSet* ts, const char* tok);
 int isMember(TokenSet* ts, const char* tok);
+
+#endif
